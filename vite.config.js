52ad505ac3db54963d42fd0417e43d4e2a712e10
Geoff Webster <gawebster@me.com>
2025-06-12 15:31:59 +0100

    🧪 Use esbuild-wasm for Vercel compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 // MIT License © 2025 Geoff Webster
 // Gorstan v2.5
-process.env.ESBUILD_BINARY_PATH = require.resolve('esbuild-wasm/bin/esbuild.wasm');
 
 import { defineConfig } from 'vite';
-import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
-
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-plugins: [react(), wasm(), topLevelAwait()]
+plugins: [react()]
 });
